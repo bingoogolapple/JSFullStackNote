59e238d0a89b010b0cf9baad14e74fcef88a72d3
bingoogolapple <bingoogolapple@gmail.com>
2016-08-20 02:14:33 +0800

    测试端口映射

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 var http = require('http');
-var requestHandler = function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello Docker Node DaoCloud 测试自动发布\n');
-}
-var web = http.createServer(requestHandler);
-web.listen(8888);
+
+var server = http.createServer(function(req, res) {
+    //设置头部
+    res.writeHead(200,{"Content-Type":"text/html;charset=UTF8"});
+    res.write("<h1>Hello Docker Node DaoCloud</h1>");
+    res.write("<h2>测试自动发布</h2>");
+    res.end("<h2>测试端口映射</h2>");
+});
+
+server.listen(8888);
+
 console.log('Server running at http://localhost:8888');
