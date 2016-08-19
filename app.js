d85e78b615ba41aec0810c05adc33822651b8f42
bingoogolapple <bingoogolapple@gmail.com>
2016-08-20 01:28:33 +0800

    测试自动发布

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var requestHandler = function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello Docker Node DaoCloud\n');
+    res.end('Hello Docker Node DaoCloud 测试自动发布\n');
 }
 var web = http.createServer(requestHandler);
 web.listen(80);
